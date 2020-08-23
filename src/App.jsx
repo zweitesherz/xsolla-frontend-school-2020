789b436769f497de5831c615e3f406fe6635ee18
Maria <zweitesherz@mail.ru>
2020-08-23 19:15:18 +0500

    генерация карточек и отрисовка с помощью faker js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, {Fragment} from 'react';
+import faker from 'Faker';
+
 
 import styles from './styles.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Concerts from "./Concerts";
 
 import axios from 'axios';
@@ -15,23 +17,41 @@ export default class PersonList extends React.Component {
     }
 
 
-
     componentDidMount() {
-        axios.get('/cards.json')
-            .then(res => {
-                const people= res.data;
-                this.setState({ people});
-            })
+        // axios.get('/cards.json')
+        //     .then(res => {
+        //         const people= res.data;
+        //         this.setState({ people});
+        //     })
+
+
+        const generate = Array(12).fill().map(( a,i) => ({
+
+            "id": i,
+            "name": faker.Lorem.words(1).toString(),
+            "date": "11",
+            "city": faker.Address.city(),
+            "genre": faker.Lorem.words(1).toString(),
+            "image": `${faker.Image.abstractImage()}?random=${Date.now()}`
+        }))
+        //console.log(generate)
+       //const people = JSON.stringify(generate,null,' ')
+
+        const people = generate;
+
+        this.setState({ people});
+        //this.state.people = generate;
+
     }
 
     render() {
-        const concert =   this.state.people.map(person =>
-            person
-        )
+        // const concert =   this.state.people.map(person =>
+        //     person
+        // )
+        console.log( this.state.people)
         return (
 
             <BrowserRouter>
-
                 <div className={styles.wrapperFull}>
                     {this.state.people.map(person => (
                     <Route exact path={`/link/${person.id}`}  key={person.id}>
